a9d3cad66bcb0b6841c8545451b7c95a814fdd93
darkfitch <darkfitch@163.com>
2018-10-15 20:08:02 +0800

    添加一点

--- main.py
+++ main.py
@@ -6,50 +6,23 @@ import traceback
 
 
 class TreeView(object):
+    '''
+    主目录,可以实现指定目录下子目录文件的树形结构展示
+    '''
 
     def __init__(self):
         self.tree = {}
         self.name = ''
 
-    # def presentation(self,path):
-    #     file_list = list(os.walk(path))
-    #     self.path = os.path.basename(path)
-    #
-    #     ret = self.file_list_loop(file_list)
-    #     # for file in ret[0]:
-    #     #     # if os.path.isfile()
-    #     #     pass
-    #
-    # def file_list_loop(self,files):
-    #     level = 0
-    #     for file_tuple in files:
-    #         print(file_tuple)
-    #         level += 1
-    #         dir_list = file_tuple[1]
-    #         file_list = file_tuple[2]
-    #         father_name = os.path.basename(file_tuple[0])
-    #         self.tree[str(level)] = {'dir_list':dir_list,'file_list':file_list,'father_name':father_name}
-    #     print(self.tree)
-    #     return (level)
-    #
-    # def serialize(self,args):
-    #     tree_view = {}
-    #     for level in len(args):
-    #         dir_list = []
-    #         file_list = []
-    #         ret = args[str(level+1)]
-    #         father_name = ret['father_name']
-    #         if father_name == self.name:
-    #             for dir in ret[dir_list]:
-    #                 tree_view[father_name] = dir_list
-
     def presentation(self, path,method='con'):
-        ret = [os.path.basename(path)]
-        index = 0
-        ret = self.file_list_loop(path,index,ret)
-        if method == 'con':
-            self.console_display(ret)
-
+        try:
+            ret = [os.path.basename(path)]
+            index = 0
+            ret = self.file_list_loop(path,index,ret)
+            if method == 'con':
+                self.console_display(ret)
+        except FileNotFoundError:
+            print('未发现该文件夹/文件,请检查后重试')
 
     def file_list_loop(self,path,index,ret):
         file_list = os.listdir(path)
@@ -58,10 +31,9 @@ class TreeView(object):
             index += 1
             format_str = '--' *index
             space = '  '
-            if os.path.isfile(file_path):
-                ret.append(f'|{format_str}{space}{file}')
-            else:
-                ret.append(f'|{format_str}{space}{file}')
+            format_str = f'|{format_str}{space}{file}'
+            ret.append(format_str)
+            if not os.path.isfile(file_path):
                 self.file_list_loop(file_path,index,ret)
         return ret
 
@@ -69,7 +41,17 @@ class TreeView(object):
         for arg in args:
             print(arg)
 
+    def file_display(self,args,tag_file_path='',file_name=''):
+        try:
+            if os.path.isfile(tag_file_path):
+            pass
+        except:
+            pass
+
+    def _write(self,name,path):
+        with open
+
 if __name__ == '__main__':
-    path = 'E:\git_code\\treeview_demo'
+    path = 'E:\_projects\document\\1~25-ly'
     tree_view = TreeView()
     tree_view.presentation(path)
