b8251c912af53dec9ed9936de8e16b6ef423a609
darkfitch <872924693@qq.com>
2018-10-16 00:33:02 +0800

    commit a little

--- main.py
+++ main.py
@@ -17,10 +17,14 @@ class TreeView(object):
     def presentation(self, path,method='con'):
         try:
             ret = [os.path.basename(path)]
-            index = 0
-            ret = self.file_list_loop(path,index,ret)
-            if method == 'con':
-                self.console_display(ret)
+            print(path)
+            if os.path.isdir(path):
+                index = 0
+                ret = self.file_list_loop(path,index,ret)
+                if method == 'con':
+                    self.console_display(ret)
+                elif method == 'file':
+                    self.file_display(ret,file_name='ret')
         except FileNotFoundError:
             print('未发现该文件夹/文件,请检查后重试')
 
@@ -29,7 +33,7 @@ class TreeView(object):
         for file in file_list:
             file_path = os.path.join(path,file)
             index += 1
-            format_str = '--' *index
+            format_str = '-' *index
             space = '  '
             format_str = f'|{format_str}{space}{file}'
             ret.append(format_str)
@@ -43,15 +47,23 @@ class TreeView(object):
 
     def file_display(self,args,tag_file_path='',file_name=''):
         try:
-            if os.path.isfile(tag_file_path):
-            pass
+            # if os.path.isfile(tag_file_path):
+                # self._write(args,file_name,tag_file_path)
+            self._write(args,file_name,tag_file_path)
         except:
             pass
 
-    def _write(self,name,path):
-        with open
+    def _write(self,args,name,path=''):
+        full_name = os.path.join(path,name)
+        print('start write')
+        with open(full_name,'w+') as f:
+            for line in args:
+                f.write(line)
+                f.write('\n')
+
+
 
 if __name__ == '__main__':
-    path = 'E:\_projects\document\\1~25-ly'
+    path = r'E:\git_code\treeview_demo'
     tree_view = TreeView()
-    tree_view.presentation(path)
+    tree_view.presentation(path,method='file')
